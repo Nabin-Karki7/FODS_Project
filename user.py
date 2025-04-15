5496d46607a1e5c222ee6622d14c8afc7020ff6f
Nabin Karki <nabinkarkii007@gmail.com>
2025-04-15 23:26:03 +0545

    Add files via upload

---
+++ user.py
@@ -0,0 +1,11 @@
+# This portion was done by Nabin
+# Initialize user information
+class User:
+    def __init__(self,id,name,role):
+        self.id=id
+        self.name=name
+        self.role=role
+
+# Display profile information in student dashboard
+    def viewprofile(self):
+        return f"ID: {self.id}, Name: {self.name}, Role: {self.role}"
